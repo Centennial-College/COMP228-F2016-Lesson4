b6aa8fba17248ca73c3dfc942d11ce1e92d9cc2a
Kevin Ma <kbma.kevin@gmail.com>
2016-09-28 09:35:04 -0400

    Example 2 - MemberAccessTest

---
+++ src/wk4_lecture/MemberAccessTest.java
@@ -0,0 +1,14 @@
+package wk4_lecture;
+
+public class MemberAccessTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Time1 time = new Time1();
+
+		time.hour = 7;
+		time.minute = 15;
+		time.seconds = 30;
+	}
+
+}
