3ce2cc7b865ba59703a79b7c97499741f3d9fddc
Kevin Ma <kbma.kevin@gmail.com>
2016-09-30 19:39:44 -0400

    Static Import Example

---
+++ src/wk4_lab/StaticImportTest.java
@@ -0,0 +1,13 @@
+package wk4_lab;
+
+import static java.lang.Math.*;
+
+public class StaticImportTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("sqrt(900.0) = %.1f%n", sqrt(900.0));
+		System.out.printf("ceil(-9.8) = %.1f%n", ceil(-9.8));
+	}
+
+}
